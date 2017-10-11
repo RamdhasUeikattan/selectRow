6f9bac1b65b26660935a71e96ba3462116d298ac
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-10-11 14:04:31 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { gridData1 } from './data';
-import { GridComponent, RowSelectEventArgs, EditSettingsModel } from '@syncfusion/ej2-ng-grids';
+import { GridComponent } from '@syncfusion/ej2-ng-grids';
 
 @Component({
     selector: 'my-app',
     template: `
     <input (change)='change($event)'>
-    <ej-grid #grid [dataSource]='data' height='315px' [editSettings]='editsetting' [toolbar]='toolbar' (actionComplete)='action($event)'>
+    <ej-grid #grid [dataSource]='data' height='315px'>
                 <e-columns>
-                    <e-column field='OrderID' [isPrimaryKey]='true' headerText='Order ID' textAlign='right' width=120></e-column>
+                    <e-column field='OrderID' headerText='Order ID' textAlign='right' width=120></e-column>
                     <e-column field='CustomerID' headerText='Customer ID' width=150></e-column>
                     <e-column field='ShipCity' headerText='Ship City' width=150 [visible]='false'></e-column>
                     <e-column field='ShipName' headerText='Ship Name' width=150></e-column>
@@ -19,25 +19,15 @@ import { GridComponent, RowSelectEventArgs, EditSettingsModel } from '@syncfusio
 export class AppComponent implements OnInit {
 
     public data: Object[];
-    public editsetting: EditSettingsModel;
-    public toolbar;
 
     @ViewChild('grid')
     public grid: GridComponent;
 
     ngOnInit(): void {
-        this.toolbar = ['add', 'edit', 'delete', 'update', 'cancel'];
-        this.editsetting = {allowEditing: true, allowAdding: true, allowDeleting: true};
         this.data = gridData1;
     }
 
     change(args: Event){
         this.grid.selectRow(parseInt((<HTMLInputElement>args.target).value, 10));
     }
-
-    // rowSelected(args: RowSelectEventArgs) {
-    //     let selectedrowindex: number[] = this.grid.getSelectedRowIndexes();  // Get the selected row indexes.
-    //     let selectedrecords: Object[] = this.grid.getSelectedRecords();  // Get the selected records.
-    //     alert(selectedrowindex + '\n' +''); // To alert the selected row indexes.
-    // }
 }
