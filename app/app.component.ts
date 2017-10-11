1cfc9464e27988c61cd0a14a5f37d6e22a3647c2
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-10-11 16:46:45 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,13 @@ import { GridComponent } from '@syncfusion/ej2-ng-grids';
 @Component({
     selector: 'my-app',
     template: `
-    <input (change)='change($event)' placeholder='Enter Row index'>
+    <input type='text' [(ngModel)]='index'>
+    <button ej-button (click)=click()>select row</button>
     <ej-grid #grid [dataSource]='data' height='415px'>
                 <e-columns>
                     <e-column field='OrderID' headerText='Order ID' textAlign='right' width=120></e-column>
                     <e-column field='CustomerID' headerText='Customer ID' width=150></e-column>
-                    <e-column field='ShipCity' headerText='Ship City' width=150 [visible]='false'></e-column>
+                    <e-column field='ShipCity' headerText='Ship City' width=150 ></e-column>
                     <e-column field='ShipName' headerText='Ship Name' width=150></e-column>
                 </e-columns>
                 </ej-grid>
@@ -19,15 +20,18 @@ import { GridComponent } from '@syncfusion/ej2-ng-grids';
 export class AppComponent implements OnInit {
 
     public data: Object[];
+    public index: number =0;
 
     @ViewChild('grid')
     public grid: GridComponent;
-
+   
     ngOnInit(): void {
         this.data = gridData1;
     }
 
-    change(args: Event){
-        this.grid.selectRow(parseInt((<HTMLInputElement>args.target).value, 10));
+    click(){
+        if(this.grid.getSelectedRowIndexes().indexOf(this.index) === -1){
+            this.grid.selectRow(this.index);
+        }
     }
 }
